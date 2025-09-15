207f3e27dfdff7bbbdf9ba14604db5d114003423
itsmelann <168535741+Lannnnnzzzzzzzzzzz@users.noreply.github.com>
2025-09-15 11:29:46 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          900: '#111827',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
